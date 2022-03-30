6a436c3353fb6455ddea6c7241b00e61c423eb57
ScyllaSync <thantkyawswarsoe2001227133@gmail.com>
2022-03-30 14:52:37 +0630

    tkss

---
+++ vue/app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    //template: '<h2>I am the template</h2>,
+    data(){
+        return{
+            name:"Thant Kyaw Swar Soe",
+            age:21,
+        }
+    }
+})
+//console.log(app)
+app.mount("#app")
