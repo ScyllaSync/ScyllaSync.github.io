98934f28738e7d85fd56d345db1ad894e794fbcb
ScyllaSync <thantkyawswarsoe2001227133@gmail.com>
2022-04-17 19:45:42 +0630

    vue

---
+++ vue/my-project/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TestingPage/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TestingPage from './components/TestingPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TestingPage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/my-project/src/components/TestingPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <h2>My Testing Component</h2>
+    <p>{{name}}-{{age}}</p>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            name:"Thant Kyaw Swar Soe",
+            age:21
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
